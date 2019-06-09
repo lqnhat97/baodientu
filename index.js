dfa879fe1b8e0871ec32825b22a2197e4e252c80
nhatlq <lqnhat97@gmail.com>
2019-06-09 10:18:15 +0700

    update

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
-var db = require('./database/db');
+var exphbs = require('express-handlebars');
+var mainRepos = require('./repository/main');
 
 var app = express();
 
 var adminController = require('./controller/adminController');
+var userController = require('./controller/userController')
 
 app.engine('hbs', exphbs({
-    defaultLayout:'main.hbs',
-    layoutsDir:'views/layouts'
-}));
-app.engine('hbs', exphbs({
-    layoutsDir:'views/layouts'
+    layoutsDir: 'views/layouts',
+    helpers: {
+        category:()=> {
+           return mainRepos.loadChuyenMuc();
+        }
+    }
 }));
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
-app.set('view option',{layout:false})
+app.set('view option', {
+    layout: false
+})
 
 //routing
 //Trang chủ
 app.get('/', function (req, res) {
-    db.load('select * from NguoiDung').then(row=>{
-        console.log(row);
-    }).catch(e=>{
-        if(e) throw e;
-    })
-    res.render("index",{
-        layout:'main.hbs'
+    res.render("index", {
+        layout: 'main.hbs'
     });
 });
 
 //Đăng nhập
 app.get('/login', function (req, res) {
-    res.render("login",{
-        layout:'main.hbs'
+    res.render("login", {
+        layout: 'main.hbs'
     });
 });
 
 //Đăng xuất
 app.get('/signup', function (req, res) {
-    res.render("signup",{
-        layout:'main.hbs'
+    res.render("signup", {
+        layout: 'main.hbs'
     });
 });
 
 //Category
 app.get('/category', function (req, res) {
-    res.render("category",{
-        layout:'main.hbs'
+    res.render("category", {
+        layout: 'main.hbs'
     });
 });
 
 //Contact
 app.get('/contact', function (req, res) {
-    res.render("contact",{
-        layout:'main.hbs'
+    res.render("contact", {
+        layout: 'main.hbs'
     });
 });
 
-app.use('/admin',adminController);
+app.use('/admin', adminController);
+app.use('/user', userController);
 
-app.listen(3000,()=>{
+
+app.listen(3000, () => {
     console.log('connected at http://localhost:3000/')
 });
---
+++ repository/main.js
@@ -0,0 +1,6 @@
+var db = require('../database/db');
+
+exports.loadChuyenMuc=()=>{
+    let sql = 'select * from chuyenmuc';
+    return db.load(sql);
+}
