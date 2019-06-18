f1401fc89c14708928194d0045985e6afac80b6a
nhatlq <lqnhat97@gmail.com>
2019-06-18 13:12:20 +0700

    fix hien thi bai viet chi tiet

--- controller/postController.js
+++ controller/postController.js
@@ -6,30 +6,37 @@ router.get('/:idBaiViet', function (req, res) {
     let baiViet = postRepo.loadBaiViet(req.params.idBaiViet);
     let nhan = postRepo.loadNhan(req.params.idBaiViet);
     let binhLuan = postRepo.loadBinhLuan(req.params.idBaiViet);
-    Promise.all([baiViet,nhan,binhLuan]).then(([baiVietRes,nhanRes,binhLuanRes]) => {
-        let baiVietChuyenMuc = postRepo.loadBaiVietCungChuyenMuc(baiVietRes[0][0].IDChuyenMuc);
-        let random1bai = postRepo.loadRandom1Bai(baiVietRes[0][0].IDChuyenMuc);
-        Promise.all([baiVietChuyenMuc,random1bai]).then(([baiVietChuyenMucRes,random1baiRes])=>{
-            let resData = {
-                layout: 'main.hbs',
-                baiViet: baiVietRes[0][0],
-                baiVietChuyenMuc: baiVietChuyenMucRes[0],
-                random1bai:random1baiRes[0],
-                nhan:nhanRes[0],
-                binhLuan:binhLuanRes[0]
-            }
-            res.render("news-details", resData);
-        })        
+    Promise.all([baiViet, nhan, binhLuan]).then(([baiVietRes, nhanRes, binhLuanRes]) => {
+        try {
+            let baiVietChuyenMuc = postRepo.loadBaiVietCungChuyenMuc(baiVietRes[0][0].IDChuyenMuc);
+            let random1bai = postRepo.loadRandom1Bai(baiVietRes[0][0].IDChuyenMuc);
+            Promise.all([baiVietChuyenMuc, random1bai]).then(([baiVietChuyenMucRes, random1baiRes]) => {
+                let resData = {
+                    layout: 'main.hbs',
+                    baiViet: baiVietRes[0][0],
+                    baiVietChuyenMuc: baiVietChuyenMucRes[0],
+                    random1bai: random1baiRes[0],
+                    nhan: nhanRes[0],
+                    binhLuan: binhLuanRes[0]
+                }
+                res.render("news-details", resData);
+            })
+        } catch (error) {
+            console.log(error);
+            res.render('error404', {
+                layout: 'main.hbs'
+            })
+        }
     })
 
 });
 
 router.post('/:idBaiViet', function (req, res) {
-    let binhluan= req.body.noidung;
+    let binhluan = req.body.noidung;
     let id = req.params.idBaiViet
-    let nguoibinhluan =req.session.passport.user.ID
-    let binhLuan = postRepo.themBinhLuan(id,binhluan,nguoibinhluan).then(rows=>{
-        res.redirect("/post/"+id);
+    let nguoibinhluan = req.session.passport.user.ID
+    let binhLuan = postRepo.themBinhLuan(id, binhluan, nguoibinhluan).then(rows => {
+        res.redirect("/post/" + id);
     });
 });
 
